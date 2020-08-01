d2a7cf079fd5057106b7229d2311cf5a8847ea88
Mark Zbikowski <mark@zbikowski.org>
2020-08-01 08:22:04 -0700

    Adds v8/wasm_module_runner target

--- CMakeTests/tests.cmake
+++ CMakeTests/tests.cmake
@@ -31,3 +31,34 @@ add_dependencies(wasm_test_common
   v8-bytecodes-builtin-list
   v8_torque_generated
   )
+
+add_library(wasm_module_runner STATIC)
+target_sources(wasm_module_runner
+  PRIVATE
+    ${PROJECT_SOURCE_DIR}/v8/test/common/wasm/wasm-module-runner.cc
+    ${PROJECT_SOURCE_DIR}/v8/test/common/wasm/wasm-module-runner.h
+  )
+target_compile_definitions(wasm_module_runner
+  PRIVATE
+    ${v8_defines}
+    ${disable-exceptions-defines}
+    ${internal_config_base_defines}
+    ${external_config_defines}
+  )
+target_compile_options(wasm_module_runner
+  PRIVATE
+    ${disable-exceptions-flags}
+    ${internal_config_base_flags}
+    ${external_config_flags}
+  )
+target_include_directories(wasm_module_runner
+  PRIVATE
+    ${v8_includes}
+    ${internal_config_base_includes}
+    ${external_config_includes}
+  )
+target_link_libraries(wasm_module_runner PRIVATE
+  v8-bytecodes-builtin-list # generate_bytecode_builtins_list
+  v8_torque_generated # run_torque
+  # v8_tracing
+  )
